18e401ba245b1d2abbdf44e307fe2f900bf01a4f
Yuan <you@example.com>
2017-07-27 11:08:35 -0500

    image

--- classifier2.py
+++ classifier2.py
@@ -38,7 +38,7 @@ step_size = (10, 10)
 #length = tf.reshape(length,(1,1))
 
 
-filename = "test/151.png"
+filename = "test/181.png"
 strings = filename[-7:-4]
 im = imread(filename)
 img = imread(filename,as_grey=True)
